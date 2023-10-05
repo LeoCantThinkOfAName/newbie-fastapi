e04576438936088f17bbfad4dcf32c40bd138190
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-10-05 16:13:01 +0800

    feat: create new route and apply pydantic to validate request body

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Track(BaseModel):
+    title: str
+    artist: str
+    album: str | None = None
+    year: int
+    label: str | None = None
+
+
+tracks: list[Track] = []
 
 
 @app.get("/")
 async def root():
     return {"Hello": "World!"}
+
+
+@app.get("/tracks")
+async def get_tracks():
+    return tracks
+
+
+@app.post("/tracks")
+async def create_track(track: Track):
+    tracks.append(track)
+    return track
