560e6df5a39384fa1a0995505cc7a1ccee19a276
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-10-06 22:09:12 +0800

    feat: restructure folder, add path and search query

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routers import tracks
 
 app = FastAPI()
+
+fallback = {404: {"description": "Not Found"}}
+app.include_router(tracks.router, prefix="/tracks", tags=["Tracks"], responses=fallback)
 
 
 @app.get("/")
---
+++ app/routers/tracks.py
@@ -0,0 +1,77 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+
+class Track(BaseModel):
+    id: int
+    title: str
+    artist: str
+    album: str | None = None
+    year: int
+    label: str | None = None
+
+
+router = APIRouter()
+
+tracks: list[Track] = [
+    {
+        "id": 1,
+        "title": "Here comes the sun",
+        "artist": "Beatles",
+        "album": "Abby Road",
+        "year": 1969,
+        "label": "Apple Records",
+    },
+    {
+        "id": 2,
+        "title": "Song 2",
+        "artist": "Blur",
+        "album": "Blur",
+        "year": 1997,
+        "label": "Parlophone",
+    },
+    {
+        "id": 3,
+        "title": "High & Dry",
+        "artist": "Radiohead",
+        "album": "The Bends",
+        "year": 1995,
+        "label": "EMI",
+    },
+    {
+        "id": 4,
+        "title": "The Rain Song",
+        "artist": "Le Zeppelin",
+        "album": "Houses of The Holy",
+        "year": 1973,
+        "label": "Atlantic Records",
+    },
+]
+
+
+@router.get("")
+async def get_tracks(skip: int = 0, limit: int = 1, q: str | None = None):
+    matched: [Track] = []
+
+    if q:
+        for track in tracks:
+            if track["artist"].lower() == q.lower():
+                matched.append(track)
+
+    else:
+        return tracks[skip : skip + limit]
+    return matched[skip : skip + limit]
+
+
+@router.get("/{track_id}")
+async def get_track_by_id(track_id: int):
+    for track in tracks:
+        if track["id"] == track_id:
+            return track
+    raise HTTPException(status_code=404, detail="Track not found")
+
+
+@router.post("")
+async def create_track(track: Track):
+    tracks.append(track)
+    return track
