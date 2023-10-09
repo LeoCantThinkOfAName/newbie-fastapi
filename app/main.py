c2d57690d3802c81b202711cab44df81c99a359b
LeoCantThinkofAName <lctoan.com@gmail.com>
2023-10-09 23:01:53 +0800

    feat: include users route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routers import tracks
+from routers import tracks, users
 
 app = FastAPI()
 
 fallback = {404: {"description": "Not Found"}}
 app.include_router(tracks.router, prefix="/tracks", tags=["Tracks"], responses=fallback)
+app.include_router(users.router, prefix="/users", tags=["Users"], responses=fallback)
 
 
 @app.get("/")
